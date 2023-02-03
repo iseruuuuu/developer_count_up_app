df0fc007da0c6ac9807110fcd7099ed6b7c77830
iseruuuuu <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>
2023-02-03 21:17:34 +0900

    add1.0: style_constantを追加した

---
+++ lib/constants/style_constant.dart
@@ -0,0 +1,11 @@
+// Flutter imports:
+import 'package:developer_count_up_app/constants/color_constant.dart';
+import 'package:flutter/material.dart';
+
+class StyleConstant {
+  static TextStyle countTextStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 50,
+    color: ColorConstant.black,
+  );
+}
