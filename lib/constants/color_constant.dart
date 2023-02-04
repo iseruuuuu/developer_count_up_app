d329a122e08afd437aa663f10234f3964d6bb823
iseruuuuu <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>
2023-02-04 09:53:04 +0900

    add1.0: 回数によって色を変えられる準備を行なった

--- lib/constants/color_constant.dart
+++ lib/constants/color_constant.dart
@@ -4,13 +4,15 @@ import 'package:flutter/material.dart';
 class ColorConstant {
   static Color appBarColor = const Color(0xFF4A67AD);
   static Color backgroundColor = const Color(0xFFF2F2F7);
-  static Color settingsListBackground = const Color(0xFFF2F2F7);
-  static Color settingsSectionBackground = Colors.white;
+
+  //AtCoderのランク順にする。
+  static Color red = Colors.red;
+  static Color orange = Colors.orange;
   static Color yellow = Colors.yellow;
-  static Color lightBlue = Colors.lightBlue;
   static Color blue = Colors.blue;
+  static Color lightBlue = Colors.lightBlue;
+  static Color green = Colors.green;
+  static Color brown = Colors.brown;
   static Color grey = Colors.grey;
   static Color black = Colors.black;
-  static Color white = Colors.white;
-  static Color red = Colors.red;
 }
