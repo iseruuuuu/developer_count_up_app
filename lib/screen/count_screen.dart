b0e98154ebf26b8d4f3959033ea1eff2ba04f9b4
iseruuuuu <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>
2023-02-04 10:19:59 +0900

    add1.0: 保存・読み込みの処理の追加

--- lib/screen/count_screen.dart
+++ lib/screen/count_screen.dart
@@ -1,6 +1,7 @@
 import 'package:developer_count_up_app/constants/color_constant.dart';
 import 'package:developer_count_up_app/constants/style_constant.dart';
 import 'package:developer_count_up_app/extension/deviceSize.dart';
+import 'package:developer_count_up_app/preference/shared_preference.dart';
 import 'package:flutter/material.dart';
 
 class CountScreen extends StatefulWidget {
@@ -13,6 +14,13 @@ class CountScreen extends StatefulWidget {
 class _CountScreenState extends State<CountScreen> {
   int counter = 0;
   String message = '';
+  Color rankColor = ColorConstant.black;
+
+  @override
+  void initState() {
+    super.initState();
+    checkColor();
+  }
 
   void _incrementCounter() {
     if (counter % 10 == 9) {
@@ -20,16 +28,45 @@ class _CountScreenState extends State<CountScreen> {
     } else {
       message = '';
     }
-    setState(() {
-      counter++;
-    });
+    counter++;
+    checkColor();
+    setPreference();
+    setState(() {});
+  }
+
+  void checkColor() async {
+    counter = await Preference().getInt(PreferenceKey.counter);
+    if (counter >= 10) {
+      rankColor = ColorConstant.grey;
+    } else if (counter >= 300) {
+      rankColor = ColorConstant.brown;
+    } else if (counter >= 600) {
+      rankColor = ColorConstant.green;
+    } else if (counter >= 900) {
+      rankColor = ColorConstant.lightBlue;
+    } else if (counter >= 1200) {
+      rankColor = ColorConstant.blue;
+    } else if (counter >= 1500) {
+      rankColor = ColorConstant.yellow;
+    } else if (counter >= 1800) {
+      rankColor = ColorConstant.orange;
+    } else if (counter >= 2000) {
+      rankColor = ColorConstant.red;
+    } else {
+      rankColor = ColorConstant.black;
+    }
+    setState(() {});
+  }
+
+  void setPreference() {
+    Preference().setInt(PreferenceKey.counter, counter);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: ColorConstant.appBarColor,
+        backgroundColor: rankColor,
         elevation: 0,
       ),
       body: Center(
@@ -44,9 +81,9 @@ class _CountScreenState extends State<CountScreen> {
               width: context.screenWidth / 3.5,
               height: context.screenWidth / 3.5,
               child: FloatingActionButton(
-                focusColor: ColorConstant.appBarColor,
-                backgroundColor: ColorConstant.appBarColor,
-                foregroundColor: ColorConstant.appBarColor,
+                focusColor: rankColor,
+                backgroundColor: rankColor,
+                foregroundColor: rankColor,
                 onPressed: () {
                   _incrementCounter();
                 },
